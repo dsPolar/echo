a6acf30db63878d2936861944768cc021c011ccf
dsPolar <ds16797@bristol.ac.uk>
2020-01-01 16:27:30 +0000

    dataset.py changes to load correct features then concatenate to be loaded into torch

--- dataset.py
+++ dataset.py
@@ -3,6 +3,9 @@ from torch.utils import data
 import numpy as np
 import pickle
 
+#Chroma, Spectral Contrast and Tonnetz in both
+#LM in LMC
+#MFCC in MC
 
 class UrbanSound8KDataset(data.Dataset):
     def __init__(self, dataset_path, mode):
@@ -11,18 +14,38 @@ class UrbanSound8KDataset(data.Dataset):
 
     def __getitem__(self, index):
         if self.mode == 'LMC':
-            # Edit here to load and concatenate the neccessary features to 
+            lm = self.dataset['features']['logmelspec']
+            chroma = self.dataset['features']['chroma']
+            speccon = self.dataset['features']['spectral_contrast']
+            tonnetz = self.dataset['features']['Tonnetz']
+
+            feature = np.concatenate(lm,chroma,speccon,tonnetz, axis=0)
+
+            # Edit here to load and concatenate the neccessary features to
             # create the LMC feature
             feature = torch.from_numpy(feature.astype(np.float32)).unsqueeze(0)
         elif self.mode == 'MC':
-            # Edit here to load and concatenate the neccessary features to 
+            mc = self.dataset['features']['mfcc']
+            chroma = self.dataset['features']['chroma']
+            speccon = self.dataset['features']['spectral_contrast']
+            tonnetz = self.dataset['features']['Tonnetz']
+
+            feature = np.concatenate(mc,chroma,speccon,tonnetz, axis=0)
+            # Edit here to load and concatenate the neccessary features to
             # create the MC feature
             feature = torch.from_numpy(feature.astype(np.float32)).unsqueeze(0)
         elif self.mode == 'MLMC':
-            # Edit here to load and concatenate the neccessary features to 
+            lm = self.dataset['features']['logmelspec']
+            mc = self.dataset['features']['mfcc']
+            chroma = self.dataset['features']['chroma']
+            speccon = self.dataset['features']['spectral_contrast']
+            tonnetz = self.dataset['features']['Tonnetz']
+
+            feature = np.concatenate(lm,mc,chroma,speccon,tonnetz, axis=0)
+            # Edit here to load and concatenate the neccessary features to
             # create the MLMC feature
             feature = torch.from_numpy(feature.astype(np.float32)).unsqueeze(0)
-       
+
         label = self.dataset[index]['classID']
         fname = self.dataset[index]['filename']
         return feature, label, fname
