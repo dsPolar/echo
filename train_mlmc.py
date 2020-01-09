84b31c607823b9b3ecf4ce795ba97dc1cd36e051
dsPolar <ds16797@bristol.ac.uk>
2020-01-09 11:16:11 +0000

    removed torchvision import

--- train_mlmc.py
+++ train_mlmc.py
@@ -8,7 +8,6 @@ import torch.backends.cudnn
 import numpy as np
 from torch import nn, optim
 from torch.nn import functional as F
-import torchvision.datasets
 from torch.optim.optimizer import Optimizer
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
